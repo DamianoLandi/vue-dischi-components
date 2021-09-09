b95bf6377020e62855ccc6122e23d6a74fea818d
DamianoLandi <damianolandi01@gmail.com>
2021-09-09 16:36:17 +0200

    Creazione, Link e Importazione

---
+++ src/components/MainContent.vue
@@ -0,0 +1,32 @@
+<template>
+    <SongCard/>
+</template>
+
+<script>
+import axios from "axios"
+
+import SongCard from "./SongCard.vue"
+export default {
+    name: "MainContent",
+    components: {
+        SongCard
+    },
+    data(){
+        return{
+            songList: null,
+        }
+    },
+    created(){
+         axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) => {
+            const response = res.data.response;
+            this.songList = response;
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SongCard.vue
@@ -0,0 +1,13 @@
+<template>
+ <h2>ciao</h2>
+</template>
+
+<script>
+export default {
+    name: "SongCard"
+}
+</script>
+
+<style>
+
+</style>
